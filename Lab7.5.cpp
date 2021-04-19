55aafad44fab0e43998e020986a97d059afc329f
MaximKolesnikSBS <79437045+MaximKolesnikSBS@users.noreply.github.com>
2021-04-19 22:00:09 +0600

    Add files via upload

---
+++ Lab7.5.cpp
@@ -0,0 +1,64 @@
+﻿/*
+	Автор: Максим Колесник
+	Группа: СБС-001-О-01
+	Задание№: 7: 5)
+	Цель: Реализовать функцию для вычисления гиперболического синуса(граница ряда определяется как константа).
+*/
+
+#include <iostream>
+#include <climits>
+#include <cmath>
+#define _USE_MATH_DEFINES
+using namespace std;
+
+class SimpleClass
+{
+public:
+	static int x;
+	void show();
+} obj1;
+int SimpleClass::x;
+
+int value() 
+{
+	while (true)
+	{
+		cout << "Enter the argument: ";
+		short obj1;
+		cin >> obj1;
+		if (cin.fail()) 
+		{
+			cin.clear();
+			cin.ignore(10, '\n');
+			cout << "The entered value is not correct, please try again " << endl;
+		}
+		else 
+		{
+			return obj1;
+		}
+	}
+}
+
+int factorial(int n) 
+{
+	return (n == 0) ? 1 : n * factorial(n - 1);
+}
+
+double calculations(int obj1)
+{
+	double sh = 0;
+	const unsigned short n = 8;
+	for (int i = 0; i < n; i++) 
+	{
+		sh += pow(obj1, 2 * i) / factorial(2 * i);
+	}
+	return sh;
+}
+
+int main() {
+	int obj1 = value();
+	double sh = calculations(obj1);
+	cout << "Hyperbolic sine sh(" << obj1 << ") = " << sh << endl;
+	system("pause");
+	return 0;
+}
